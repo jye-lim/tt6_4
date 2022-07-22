368102efae5fdef1b700601a8f3bf2df5a1d33cc
theKuanWay <liewkw94@gmail.com>
2022-07-22 11:44:18 +0800

    added login info, username password

--- dbshack/src/Components/Log in page/homepage.js
+++ dbshack/src/Components/Log in page/homepage.js
@@ -6,7 +6,19 @@ function Homepage (){
      // React States
   const [errorMessages, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
-  
+
+  // User Login info hardcode
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+
     return (
         <div></div>
     );
