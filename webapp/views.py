a9444d32ba89eac2de5b9904b75d55d8ee44ee75
jye-lim <jianye_lim@outlook.com>
2022-07-22 17:11:10 +0800

    updated views.py

--- webapp/views.py
+++ webapp/views.py
@@ -33,7 +33,7 @@ def home_page():
     for e in exchange_rates:
         print(e)
 
-    return render_template('home.html')
+    return redirect('http://localhost:5000/login')
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -49,13 +49,13 @@ def register_page():
         db.session.commit()
         login_user(user_to_create)
         flash(f'Account created successfully! You are now logged in as {user_to_create.username}', category='success')
-        return render_template('register.html')
+        return redirect('http://localhost:5000/login')
 
     if form.errors != {}: 
         for err_msg in form.errors.values(): 
             flash(f'The following error has occured when creating user: {err_msg}', category='danger')
 
-    return render_template('register.html', form=form)
+    return redirect('http://localhost:5000/register')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -66,18 +66,18 @@ def login_page():
         if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
             login_user(attempted_user)
             flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
-            return redirect(url_for('upload_page'))
+            return form.id 
         else: 
             flash('Username and password do not match! Please try again.', category='danger')
 
-    return render_template('login.html', form=form)
+    return False 
 
 
 @app.route('/logout')
 def logout_page():
     logout_user()
     flash("You have been logged out!", category='info')
-    return redirect(url_for("home_page"))
+    return redirect("http://localhost:5000/login")
 
 
 @app.route('/wallets')
@@ -85,7 +85,7 @@ def wallets():
     user = User.query.filter_by(obj_id=1)
     print(user[0])
     user_wallets = Wallet.query.filter_by(user_id=user[0].obj_id)
-    return render_template('wallets.html', wallets=user_wallets)
+    return redirect('http://localhost:5000/dashboard')
 
 
 @app.route('/wallets/<wallet_id>')
@@ -93,4 +93,4 @@ def view_transactions(wallet_id):
     wallet = Wallet.query.filter_by(obj_id=wallet_id).first()
     transactions = Transaction.query.filter_by(wallet_id=wallet_id)
     transactions = [t for t in transactions]
-    return render_template('transactions.html', wallet=wallet, transactions=transactions)
+    return redirect('http://localhost:5000/transactions')
