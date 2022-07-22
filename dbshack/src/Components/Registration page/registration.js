0424afef8a5bbade125d15061b7bc76aa8534b85
theKuanWay <liewkw94@gmail.com>
2022-07-22 14:27:56 +0800

    edit paramters

--- dbshack/src/Components/Registration page/registration.js
+++ dbshack/src/Components/Registration page/registration.js
@@ -8,7 +8,7 @@ function Register (){
                 <input type="text" id="name" minlength="1" required />
                 <br></br>
                 <label>Username:</label>
-                <input type="text" id="username" name="username" minlength="6" required />
+                <input type="text" id="username" name="username" minlength="3" required />
                 <br></br>
                 <label>Password:</label>
                 <input type="password" id="pwd" name="pwd" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required />
