9182c0e43752732689d1a738e041ef6a5b9dbba1
Desmond <poonweijun@gmail.com>
2022-07-22 14:09:18 +0800

    Populate database

--- webapp/models.py
+++ webapp/models.py
@@ -1,21 +1,25 @@
 from webapp import db 
 
+
 class User(db.Model): 
     id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(length=16), nullable=False, unique=True)
-    password = db.Column(db.String(length=128), nullable=False)
-    name = db.Column(db.String(length=128), nullable=False)
+    username = db.Column(db.String(length=20), nullable=False, unique=True)
+    password = db.Column(db.String(length=255), nullable=False)
+    name = db.Column(db.String(length=255), nullable=False)
+
 
 class Wallet(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     user_id = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable=False)
-    name = db.Column(db.String(length=128))
+    name = db.Column(db.String(length=255))
+
 
 class Currency(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     wallet_id = db.Column(db.Integer(), db.ForeignKey('wallet.id'), nullable=False)
     currency = db.Column(db.String(length=3), nullable=False)
     amount = db.Column(db.Float(), nullable=False)
+
 
 class Transaction(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -26,8 +30,8 @@ class Transaction(db.Model):
     credit_id = db.Column(db.Integer(), nullable=False)
     credit_currency = db.Column(db.String(length=3), nullable=False)
     credit_amount = db.Column(db.Float(), nullable=False)
-    description = db.Column(db.String(length=128))
+    description = db.Column(db.String(length=255))
     created_at = db.Column(db.String(length=128))
-    created_by = db.Column(db.String(length=128))
-    updated_at = db.Column(db.String(length=128))
-    updated_by = db.Column(db.String(length=128))
+    created_by = db.Column(db.String(length=255))
+    updated_at = db.Column(db.String(length=255))
+    updated_by = db.Column(db.String(length=255))
