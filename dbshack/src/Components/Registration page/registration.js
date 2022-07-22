a2de7d585011d803f52c7bb52deefc4cbb98a008
theKuanWay <liewkw94@gmail.com>
2022-07-22 14:51:34 +0800

    change classnames

--- dbshack/src/Components/Registration page/registration.js
+++ dbshack/src/Components/Registration page/registration.js
@@ -6,20 +6,20 @@ function Register (){
 
     return  (
         <div className="app">
-            <form>
+            <form className="form">
             <label>Name:</label>
                 <input type="text" className="name" minlength="1" required />
                 <br></br>
                 <label>Username:</label>
-                <input type="text" id="username" name="username" minlength="3" required />
+                <input type="text" className="username" name="username" minlength="3" required />
                 <br></br>
                 <label>Password:</label>
-                <input type="password" id="pwd" name="pwd" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required />
+                <input type="password" className="pwd" name="pwd" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required />
                 <br></br>
                 <label>Confirm password:</label>
-                <input type="password" id="confirm" name="confirm" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required />
+                <input type="password" className="confirm" name="confirm" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required />
                 <br></br>
-                <input type="submit" value="Register" onClick= {popout}/>
+                <input type="submit" className="submit"  value="Register" onClick= {popout}/>
             </form>
         </div>
     )
