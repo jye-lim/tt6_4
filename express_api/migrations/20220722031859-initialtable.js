cca5aeb42d19f594e7945d4dc8b34d6048bd6152
paulyeo23 <78999327+paulyeo23@users.noreply.github.com>
2022-07-22 12:34:41 +0800

    psql tables set

--- express_api/migrations/20220722031859-initialtable.js
+++ express_api/migrations/20220722031859-initialtable.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("User", {
+    await queryInterface.createTable("users", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -19,12 +19,39 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING,
       },
-      displayName: {
+      name: {
         allowNull: false,
         type: Sequelize.STRING,
         unique: true,
       },
-      country: {
+      createdAt: {
+        type: "TIMESTAMP",
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        type: "TIMESTAMP",
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+
+    await queryInterface.createTable("wallets", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      name: {
         allowNull: false,
         type: Sequelize.STRING,
       },
@@ -35,6 +62,155 @@ module.exports = {
       updatedAt: {
         type: "TIMESTAMP",
         defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+    await queryInterface.createTable("exchangeRates", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+        type: Sequelize.INTEGER,
+      },
+
+      baseCurrency: {
+        allowNull: false,
+        type: Sequelize.STRING(3),
+      },
+
+      exhangeCurrency: {
+        allowNull: false,
+        type: Sequelize.STRING(3),
+      },
+
+      rate: {
+        allowNull: false,
+        type: Sequelize.FLOAT(3),
+      },
+
+      createdAt: {
+        type: "TIMESTAMP",
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        type: "TIMESTAMP",
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+
+    await queryInterface.createTable("transactions", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+        type: Sequelize.INTEGER,
+      },
+      walletId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "wallets",
+          key: "id",
+        },
+      },
+      debitId: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+      },
+
+      debitCurrency: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "exchangeRates",
+          key: "id",
+        },
+      },
+
+      debitAmount: {
+        allowNull: false,
+        type: Sequelize.FLOAT,
+      },
+
+      creditId: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+      },
+
+      creditCurrency: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "exchangeRates",
+          key: "id",
+        },
+      },
+
+      creditAmount: {
+        allowNull: false,
+        type: Sequelize.FLOAT,
+      },
+
+      description: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        type: "TIMESTAMP",
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      createdBy: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      updatedAt: {
+        type: "TIMESTAMP",
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedBy: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+    await queryInterface.createTable("currencys", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+        type: Sequelize.INTEGER,
+      },
+      walletId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "wallets",
+          key: "id",
+        },
+      },
+      currency: {
+        allowNull: false,
+        type: Sequelize.STRING(3),
+      },
+      amount: {
+        allowNull: false,
+        type: Sequelize.FLOAT,
+      },
+      createdAt: {
+        type: "TIMESTAMP",
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      createdBy: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      updatedAt: {
+        type: "TIMESTAMP",
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedBy: {
+        allowNull: false,
+        type: Sequelize.STRING,
       },
     });
   },
