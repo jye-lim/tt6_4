9df8aed4e6c10fb90b2690ba1f19f2864fc7bb58
Luna Ong <ong.sze.teng@gmail.com>
2022-07-22 16:08:07 +0800

    updating all files

---
+++ dbs-hack/src/components/BackButton.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import '../index.css'
+
+function BackButton() {
+  return (
+    <div className="backbtndiv">
+    <button className="backbtn"><img src='../images/backbtn.png'/></button>
+    </div>
+  );
+}
+
+export default BackButton;
---
+++ dbs-hack/src/screens/Wallet.js
@@ -0,0 +1,67 @@
+import React from "react";
+import moment from "moment";
+import '../index.css'
+import BackButton from "../components/BackButton";
+
+function Wallet() {
+    var created_at = "2021-11-04 16:00:00"
+    var name = "Multi-Currency Account";
+    var amount = 123
+    var currency = "SGD"
+    var debit_amount = 111
+    var debit_currency = "EUR"
+    var credit_amount = 222
+    var credit_currency = "NOK"
+    
+  return (
+    <div className="walletdashboard">
+        <BackButton/>
+        <p>Name of wallet: {name}</p>
+        <button className="convertcurrencybtn">Convert Currency</button>
+        <button className="deletewallet">Delete Wallet</button>
+        <div className="balandtrans">
+            <div className="baldiv">
+                <p className="baltitle">Balance</p>
+                <div className="balcontent">
+                    <div className="amtcontent">
+                        <p>Amount</p>
+                        <p>{amount}</p>
+                    </div>
+                    <div className="curcontent">
+                        <p>Currency</p>
+                        <p>{currency}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="transdiv">
+                <p className="transtitle">Transactions</p>
+                <div className="transcontent">
+                    <div className="transamtcontent">
+                        <p>Amount</p>
+                        <p>{debit_amount}</p>
+                    </div>
+                    <div className="transcurcontent">
+                        <p>Currency</p>
+                        <p>{debit_currency}</p>
+                    </div>
+                    <img src="../images/arrow.png"/>
+                    <div className="transamtcontent">
+                        <p>Amount</p>
+                        <p>{credit_amount}</p>
+                    </div>
+                    <div className="transcurcontent">
+                        <p>Currency</p>
+                        <p>{credit_currency}</p>
+                    </div>
+                    <div className="datecontent">
+                        <p>Date</p>
+                        <p>{moment(created_at).format('D/MM/Y')}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Wallet;
